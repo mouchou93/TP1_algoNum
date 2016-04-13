a79e54244d8d9fbb91c95b0239174260083a3b27
Valentin <valentin@macbook-pro-de-valentin.local>
2016-04-13 14:39:33 +0200

    Initial Commit

---
+++ TP1_AlgoNum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TP1_AlgoNum
+//
+//  Created by Valentin on 13/04/2016.
+//  Copyright © 2016 Valentin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
